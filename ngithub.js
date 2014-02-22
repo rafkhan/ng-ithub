0dde768e09b1cb9b9b02543c462eb1e79b524a11
Rafael Khan <raf.khan.f@gmail.com>
2014-02-21 21:37:38 -0500

    Added access token, watches userinput ngmodel for queries

--- ngithub.js
+++ ngithub.js
@@ -1,5 +1,5 @@
-var ngithub = function($http, $compile) {
-  var API_BASE = 'https://api.github.com'
+var ngithub = function($http) {
+  var API_BASE = 'https://api.github.com';
 
   return {
     transclude: true,
@@ -7,17 +7,19 @@ var ngithub = function($http, $compile) {
 
     link: function(scope, iElement, iAttrs, ctrl, transclude) {
       scope.gh_success = true;
+    
+      scope.$watch('userinput', function() {
+        var gh_user = iAttrs.ghUser;
+        scope.gh_user = gh_user;
 
-      var gh_user = iAttrs.ghUser;
-      scope.gh_user = gh_user;
-
-      $http.get(API_BASE + '/users/' + scope.gh_user + '/repos')
-      .success(function(data, status, headers, config) {
-        scope.gh_repos = data;
-        scope.gh_success = true;
-      })
-      .error(function(data, status, headers, config) {
-        scope.gh_success = false;
+        $http.get(API_BASE + '/users/' + scope.gh_user + '/repos?access_token=540b89167bcfc693567cfe9d177b66561db449f0')
+        .success(function(data, status, headers, config) {
+          scope.gh_repos = data;
+          scope.gh_success = true;
+        })
+        .error(function(data, status, headers, config) {
+          scope.gh_success = false;
+        });
       });
     }
   };
