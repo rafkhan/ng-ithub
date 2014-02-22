e33e2b98450f1e098f3679f36aca594201798502
Rafael Khan <raf.khan.f@gmail.com>
2014-02-21 20:51:33 -0500

    stuff.

--- ngithub.js
+++ ngithub.js
@@ -1,20 +1,23 @@
-var ngithub = function($http) {
+var ngithub = function($http, $compile) {
   var API_BASE = 'https://api.github.com'
 
   return {
     transclude: true,
+    restrict: "EA",
 
-    link: function(scope, iElement, iAttrs) {
+    link: function(scope, iElement, iAttrs, ctrl, transclude) {
+      scope.gh_success = true;
 
       var gh_user = iAttrs.ghUser;
       scope.gh_user = gh_user;
 
       $http.get(API_BASE + '/users/' + scope.gh_user + '/repos')
       .success(function(data, status, headers, config) {
-        scope.ghrepos = data;
+        scope.gh_repos = data;
+        scope.gh_success = true;
       })
       .error(function(data, status, headers, config) {
-        console.log(data, status, headers, config);
+        scope.gh_success = false;
       });
     }
   };
